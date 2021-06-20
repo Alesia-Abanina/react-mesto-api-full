aa68c01f8dd9ad84c9222744e5ddc5d677689dde
Alesia Abanina <alesia.abanina@gmail.com>
2021-06-20 10:31:20 +1000

    feat: add request and error logging

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const { login, createUser } = require('./controllers/users');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,6 +22,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -44,6 +47,8 @@ app.use('/cards', cardsRouter);
 app.use((req, res) => res.status(404).send({
   message: 'Ресурс не найден. Проверьте URL и метод запроса',
 }));
+
+app.use(errorLogger);
 
 app.use(errors());
 
