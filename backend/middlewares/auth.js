f62ee119158dd86078ec332a4f366bf7146a753c
Alesia Abanina <alesia.abanina@gmail.com>
2021-06-24 21:14:47 +1000

    fix: add diagnostic message

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-
+  console.log(`token='${token}'`);
   if (!token) {
     res.status(401).send({ message: 'Необходима авторизация' });
     return;
